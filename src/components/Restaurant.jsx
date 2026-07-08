52ebed99cb59c8b57e5186d6ad6ac143eac4c812
Moshe Lev Ceder <navkc11@gmail.com>
2026-07-08 20:26:50 +0300

    feat: display number of people in the restaurant in div

--- src/components/Restaurant.jsx
+++ src/components/Restaurant.jsx
@@ -6,17 +6,13 @@ class Restaurant extends Component {
   render() {
     return (
       <div>
-        <span>
-          You have {this.props.RestaurantStore.openTables} open tables
-        </span>
-        {/* Add in # of people in restaurant */}
+        <span>You have {this.props.RestaurantStore.openTables} open tables</span>
+        <div>You have {this.props.RestaurantStore.restPopulation} people in the resturant</div>
         {/* Add in # of completed tables with id "completedTables*/}
         <ResInput />
         <button id="addRes">Add Reservation</button>
         {/* Make the Add Reservation button work */}
-        <div className="reservations">
-          {/* Map reservation data to Reservation components here */}
-        </div>
+        <div className="reservations">{/* Map reservation data to Reservation components here */}</div>
       </div>
     );
   }
